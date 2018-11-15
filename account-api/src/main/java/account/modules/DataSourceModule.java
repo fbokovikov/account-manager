7d00f3e85ce5486cc413ab8c15f9d00737c9d639
fbokovikov <fbokovikov@yandex-team.ru>
2018-11-15 17:10:39 +0300

    Use unique h2 database name for the sake of tests

--- account-api/src/main/java/account/modules/DataSourceModule.java
+++ account-api/src/main/java/account/modules/DataSourceModule.java
@@ -4,6 +4,7 @@ import com.google.inject.AbstractModule;
 import org.h2.jdbcx.JdbcDataSource;
 
 import javax.sql.DataSource;
+import java.time.Instant;
 
 /**
  * H2 Guice DataSource Module.
@@ -20,7 +21,7 @@ public class DataSourceModule extends AbstractModule {
     private DataSource h2DataSource() {
         JdbcDataSource ds = new JdbcDataSource();
         //use DB_CLOSE_DELAY=-1 to keep content after closing last connection to db
-        ds.setURL("jdbc:h2:mem:account-db;DB_CLOSE_DELAY=-1");
+        ds.setURL("jdbc:h2:mem:account-db" + Instant.now() + ";DB_CLOSE_DELAY=-1");
         ds.setUser("sa");
         ds.setPassword("sa");
         return ds;
